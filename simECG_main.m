feb56318c389a36f1c7929dcd3fbbde965e54c30
LorenzoBachi <lorenzo.bachi1@gmail.com>
2023-07-13 15:08:18 +0200

    Updated function descriptions

--- simECG_main.m
+++ simECG_main.m
@@ -1,6 +1,6 @@
 %% ECG SIMULATOR MAIN SCRIPT: USER-DEFINED PARAMETERS, OUTPUT VISUALIZATION
 
-% VERSION: 1.0.1 - July 2023, updated function description
+% VERSION: 1.0.1 - July 2023, updated function descriptions
 
 % For the scientific materials of this code, please see
 %   L. Bachi, H. Halvaei, C. Pérez, A. Martín-Yebra, A. Petrėnas,
